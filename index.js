577ab91a838f3a2f4092c485d2e0013f95f9bbac
Elijah wines <elijahwines84@yahoo.com>
2020-08-18 13:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign({}, driver, { [key]: value })
+  return Object.assign({}, driver, { [key]: value });
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-
+  driver[key] = value;
+  return driver;
 }
 
 function deleteFromDriverByKey(driver, key) {
-
+  const newdriver = {...driver};
+  delete newDriver[key];
+  return newDriver;
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
